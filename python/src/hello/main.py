94c990c553a323077ee2d50e4f0dcea3923e6e5a
Jasper Chan <jasperchan515@gmail.com>
2026-03-19 15:36:32 -0700

    python: setup pytest

--- python/src/hello/main.py
+++ python/src/hello/main.py
@@ -4,6 +4,11 @@ from psycopg2 import __libpq_version__
 def main():
     print("Hello from uv2nix python!")
     print(f"libpq version: {__libpq_version__}")
+    print(f"1 / 2 = {divide(1, 2)}")
+
+
+def divide(a, b):
+    return a / b
 
 
 if __name__ == "__main__":
