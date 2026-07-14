3acc7a920581fd1836fd07c490d7182e322457f7
Jasper Chan <jasperchan515@gmail.com>
2026-07-13 20:20:17 -0400

    esp32: add to available templates

--- flake.nix
+++ flake.nix
@@ -21,6 +21,18 @@
             TODO: FILL THIS IN
           '';
         };
+        esp32 = {
+          path = ./esp32;
+          description = "ESP32 project template";
+          welcomeText = ''
+            # Devshell
+
+            See available targets with `idf.py --list-targets`
+            Set a target with `idf.py set-target <target>`
+
+            Build, flash, monitor a connected target with `idf.py build flash monitor`
+          '';
+        };
       };
     };
 }
