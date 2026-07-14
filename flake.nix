d57a83875e8dfa446b902a9ddd5b8a1654b3602f
Jasper Chan <jasperchan515@gmail.com>
2026-07-13 20:16:47 -0400

    chore: format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,24 +1,26 @@
 {
   description = "A collection of flake templates";
 
-  outputs = {self}: {
-    templates = {
-      python = {
-        path = ./python;
-        description = "Python template, using uv2nix";
-        welcomeText = ''
-          # Getting started
-          TODO: FILL THIS IN
-        '';
-      };
-      flake-parts = {
-        path = ./flake-parts;
-        description = "Flake-parts template";
-        welcomeText = ''
-          # Getting started
-          TODO: FILL THIS IN
-        '';
+  outputs =
+    { self }:
+    {
+      templates = {
+        python = {
+          path = ./python;
+          description = "Python template, using uv2nix";
+          welcomeText = ''
+            # Getting started
+            TODO: FILL THIS IN
+          '';
+        };
+        flake-parts = {
+          path = ./flake-parts;
+          description = "Flake-parts template";
+          welcomeText = ''
+            # Getting started
+            TODO: FILL THIS IN
+          '';
+        };
       };
     };
-  };
 }
